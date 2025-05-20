c45ef340cc82690fda96d810e3b05b889d638b17
blairxu13 <spenxu2002@gmial.com>
2025-05-20 14:06:55 -0400

    updated frontend logic

--- src/popup.js
+++ src/popup.js
@@ -1,4 +1,21 @@
-document.getElementById('btn').addEventListener('click', () => {
-    alert('Button clicked!');
-  });
-  
+document.getElementById('loginBtn').addEventListener('click', () => {
+  // auth logic...
+
+  // show dashboard, hide login
+
+  document.getElementById('loginPage').style.display = 'none';
+  document.getElementById('dashboardPage').style.display = 'block';
+});
+
+document.getElementById('logoutBtn').addEventListener('click', () => {
+  // show login, hide dashboard
+  document.getElementById('dashboardPage').style.display = 'none';
+  document.getElementById('loginPage').style.display = 'block';
+});
+
+document.getElementById('trackBtn').addEventListener('click', () => {
+  // show login, hide dashboard
+  document.getElementById('dashboardPage').style.display = 'none';
+  document.getElementById('loginPage').style.display = 'none';
+  document.getElementById('activityPage').style.display = 'block';
+});
